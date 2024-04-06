216fdf2cffec08821615c994ad0c0dacd9aaad13
VyacheslavDd <Sonic.x.exe@yandex.ru>
2024-04-06 12:11:13 +0500

    простейший дизайн страницы звонящего будильника

--- components/alarm-ringing-components/ring-page/ring-page.js
+++ components/alarm-ringing-components/ring-page/ring-page.js
@@ -1,22 +1,27 @@
 
 import { View, Text, Pressable } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { commonStyles } from '../../../common-styles'
 import Gradient from '../../Gradient'
 import * as Notifications from "expo-notifications"
 import { stopAlarm } from '../../common-functions/CommonFunctions'
+import { ringPageStyles } from './ring-page-styles'
 
 export default function RingPage({navigation}) {
 
+    const [pageText, setPageText] = useState("Отключить >>")
     const stopRinging = async () => {
+        setPageText("Останавливаю...");
         await stopAlarm();
     }
 
   return (
     <Gradient>
-        <View style={commonStyles.container}>
+        <View style={[commonStyles.container, ringPageStyles.myContainer]}>
+        <Text style={ringPageStyles.wakeTime}>12:05</Text>
+        <Text style={ringPageStyles.title}>Тебе пора на первую пару!</Text>
         <Pressable onPress={async () => stopRinging()}>
-            <Text>RingPage</Text>
+            <Text style={ringPageStyles.wakeUp}>{pageText}</Text>
         </Pressable>
         </View>
     </Gradient>

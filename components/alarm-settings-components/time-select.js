5574fafaedef4b3f51683140fabbbed8d63610ed
VyacheslavDd <Sonic.x.exe@yandex.ru>
2024-04-12 21:17:03 +0500

    фикс времени

--- components/alarm-settings-components/time-select.js
+++ components/alarm-settings-components/time-select.js
@@ -43,7 +43,7 @@ export default function TimeSelect(props) {
         const [hour, minute] = timeString.split(":").map(part => Number(part));
         setCurrentHour(hour);
         setCurrentMinute(minute);
-    }, []);
+    }, [timeString]);
 
     useEffect(() => {
         setNextHour(getNextHour(currentHour));

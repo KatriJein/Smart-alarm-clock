3b825a671d75cebfae279b22ea71ed5b265c622f
KatriJein <Katrijein@yandex.ru>
2024-04-12 18:43:40 +0500

    Добавила сохранение в память телефона

--- store/store.js
+++ store/store.js
@@ -1,12 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import calendarReducer from './calendarReducer.js';
 import alarmReducer from './alarmReducer.js';
 import statisticsReducer from './statisticsReducer.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+};
+
+const reducer = combineReducers({
+  calendar: calendarReducer,
+  alarms: alarmReducer,
+  statistics: statisticsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    calendar: calendarReducer,
-    alarms: alarmReducer,
-    statistics: statisticsReducer
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export const persistor = persistStore(store);
